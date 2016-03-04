a7a0c8ffe7ee5dcb09988641cac5b20e2847cd15
David Castellanos <dcastellanos@paradigmadigital.com>
2016-03-04 13:44:20 +0100

    Generate images using both JRE and JDK

--- generate_dockerfiles.sh
+++ generate_dockerfiles.sh
@@ -11,8 +11,15 @@ for JVM_VERSION in ${JVM_VERSIONS[@]}; do
   for TOMCAT_VERSION in ${TOMCAT_VERSIONS[@]}; do
     TOMCAT_MAJOR=$(echo $TOMCAT_VERSION | cut -d. -f1)
 
-    echo -en "Generating Dockerfile for Tomcat ${TOMCAT_VERSION} using Java JRE ${JVM_VERSION}..."
-    sed "s|%JVM_VERSION%|$JVM_VERSION|g;s|%TOMCAT_VERSION%|$TOMCAT_VERSION|g;s|%TOMCAT_MAJOR%|$TOMCAT_MAJOR|g" $TEMPLATE > tomcat${TOMCAT_MAJOR}/Dockerfile.JRE${JVM_VERSION} && \
+    JVM_FLAVOUR=jre${JVM_VERSION}
+    echo -en "Generating Dockerfile for Tomcat ${TOMCAT_VERSION} using Java ${JVM_FLAVOUR}..."
+    sed "s|%JVM_FLAVOUR%|$JVM_FLAVOUR|g;s|%TOMCAT_VERSION%|$TOMCAT_VERSION|g;s|%TOMCAT_MAJOR%|$TOMCAT_MAJOR|g" $TEMPLATE > tomcat${TOMCAT_MAJOR}/Dockerfile.${JVM_FLAVOUR} && \
+      echo "done" || \
+      echo "failed"
+
+    JVM_FLAVOUR=jdk${JVM_VERSION}
+    echo -en "Generating Dockerfile for Tomcat ${TOMCAT_VERSION} using Java ${JVM_FLAVOUR}..."
+    sed "s|%JVM_FLAVOUR%|$JVM_FLAVOUR|g;s|%TOMCAT_VERSION%|$TOMCAT_VERSION|g;s|%TOMCAT_MAJOR%|$TOMCAT_MAJOR|g" $TEMPLATE > tomcat${TOMCAT_MAJOR}/Dockerfile.${JVM_FLAVOUR} && \
       echo "done" || \
       echo "failed"
   done
